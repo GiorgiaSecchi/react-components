59e66e9960139e615ad9a20e1b6034c1d3fafb05
GiorgiaSecchi <secchigrg@gmail.com>
2024-11-28 15:32:21 +0100

    add header title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,9 @@
 export default function Header() {
-  return <header className="text-center"> HEADER </header>;
+  return (
+    <header>
+      <div className="container text-center">
+        <h1>IL MIO BLOG</h1>
+      </div>
+    </header>
+  );
 }
