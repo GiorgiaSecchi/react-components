1c51f3c3ad6781d7ca97c709331aba26fca75ebc
GiorgiaSecchi <secchigrg@gmail.com>
2024-11-28 16:21:47 +0100

    add card structure

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,29 @@
 export default function Main() {
-  return <main> MAIN </main>;
+  return (
+    <main>
+      <div className="container">
+        <div className="row row-cols-1">
+          <div className="col">
+            <div className="card mx-auto">
+              <img
+                src="src\assets\img\600x400.jpg"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" class="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
