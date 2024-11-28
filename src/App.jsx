5c4ceafa987e890c68d9861f2a2c2bdeba0f672b
GiorgiaSecchi <secchigrg@gmail.com>
2024-11-28 15:00:47 +0100

    home page 3 components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 // import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
+import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
+import Footer from "./components/Footer.jsx";
 
+function App() {
   return (
     <>
-      <h1>HELLO</h1>
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
