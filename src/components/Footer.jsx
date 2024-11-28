984d94e812f218d8192438709c1a747dbe33e03b
GiorgiaSecchi <secchigrg@gmail.com>
2024-11-28 17:47:36 +0100

    mod text foot

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,7 @@ export default function Footer() {
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis
           aspernatur similique nostrum ratione, blanditiis quo cumque ut
           inventore vitae iste animi ullam! Esse est magni rem libero.
-          Voluptate, alias incidunt! Unde, eos labore veritatis dolorem ipsam
-          qui neque quibusdam maiores aliquid alias illum sed harum aut eum
-          maxime incidunt soluta animi nulla facere iusto voluptatem, laborum
-          omnis inventore expedita. Placeat?
+          Voluptate, alias incidunt!
         </p>
       </div>
     </footer>
