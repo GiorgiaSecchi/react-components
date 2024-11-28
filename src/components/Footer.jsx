cfa6566b5f335b2edc24161802a0fdf8273bb444
GiorgiaSecchi <secchigrg@gmail.com>
2024-11-28 17:44:36 +0100

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,17 @@
 export default function Footer() {
-  return <footer> FOOTER </footer>;
+  return (
+    <footer>
+      <div className="container text-center">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis
+          aspernatur similique nostrum ratione, blanditiis quo cumque ut
+          inventore vitae iste animi ullam! Esse est magni rem libero.
+          Voluptate, alias incidunt! Unde, eos labore veritatis dolorem ipsam
+          qui neque quibusdam maiores aliquid alias illum sed harum aut eum
+          maxime incidunt soluta animi nulla facere iusto voluptatem, laborum
+          omnis inventore expedita. Placeat?
+        </p>
+      </div>
+    </footer>
+  );
 }
